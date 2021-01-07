44a8849b1cf8c9c4dbb94d59e216f5958f90d489
cc-Tanaka <tanaka-ryo@colorfulcompany.co.jp>
2021-01-07 10:59:31 +0900

    tailwind使ってみる

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,29 @@
 <template>
   <Layout>
     <h1 class="font-semibold">Hello, Tailwind!</h1>
+
+    <div class="header">
+      <ul class="header__nav">
+        <li class="header__nav-item"><a class="header__nav-link">査定の流れ</a></li>
+        <li class="header__nav-item"><a class="header__nav-link">3つの強み</a></li>
+        <li class="header__nav-item"><a class="header__nav-link">よくある質問</a></li>
+      </ul>
+      <div class="header__cv-button-wrap">
+        <a class="header__cv-button">査定を申し込む</a>
+      </div>
+    </div>
+
+    <div class="h-20 flex justify-center items-center">
+      <ul class="inline-flex justify-center items-center">
+        <li class="pr-4	mr-4 border-r border-black"><a>査定の流れ</a></li>
+        <li class="pr-4	mr-4 border-r border-black"><a>3つの強み</a></li>
+        <li><a>よくある質問</a></li>
+      </ul>
+      <div class="inline-block ml-10">
+        <a class="text-sm font-bold text-white bg-yellow-400 py-3.5 px-8 rounded-full">査定を申し込む</a>
+      </div>
+    </div>
+
   </Layout>
 </template>
 
@@ -13,4 +36,37 @@ export default {
 </script>
 
 <style>
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+}
+
+.header__nav {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.header__nav-item:not(:last-child) {
+  padding-right: 16px;
+  margin-right: 16px;
+  border-right: 1px solid #333;
+}
+
+.header__cv-button-wrap {
+  display: inline-block;
+  margin-left: 40px;
+}
+
+.header__cv-button {
+  font-size: 14px;
+  font-weight: bold;
+  color: white;
+  background-color: #F0A747;
+  padding: 12px 32px;
+  border-radius: 100px;
+}
+
 </style>
